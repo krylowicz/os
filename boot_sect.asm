c752df09fab4d1c03e7053966a9a9f2f0e4b3954
krylowicz <kacper.krylowicz@uczen.zsk.poznan.pl>
2021-03-27 19:47:58 +0100

    print hello

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,6 +1,17 @@
-loop:
-  jmp loop
+mov ah, 0x0e ; tty mode
 
-times 510-($-$$) db 0
+mov al, 'H'
+int 0x10 ; interrupt
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
 
-dw 0xaa55
+jmp $ ; infinite loop
+
+times 510-($-$$) db 0 ; fill with 0 - previous code
+
+dw 0xaa55 ; boot sector number
