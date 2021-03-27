82f5fdadd01b622b34fe4375468669db9f62bf5d
krylowicz <kacper.krylowicz@uczen.zsk.poznan.pl>
2021-03-27 22:10:17 +0100

    memory offset

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,17 +1,29 @@
+[org 0x7c00] 
+; defining global offset 
+; without this 3rd attempt works and with this set now the 2nd one works
+; because it now calculates offstet from the beginning of the boot sector
+
 mov ah, 0x0e ; tty mode
 
-mov al, 'H'
-int 0x10 ; interrupt
-mov al, 'e'
+; fails because it tries to print memory addres of secret (pointer)
+mov al, secret
 int 0x10
-mov al, 'l'
+
+; this dereferences secret (like *pointer in c)
+mov al, [secret]
 int 0x10
-int 0x10
-mov al, 'o'
+
+; add the BIOS starting offset 0x7c00 to the memory addres of secret
+mov bx, secret
+add bx, 0x7c00
+mov al, [bx]
 int 0x10
 
 jmp $ ; infinite loop
 
-times 510-($-$$) db 0 ; fill with 0 - previous code
+secret:
+  db 'X' ; ASCII 'X' is stored just before zero padding
 
-dw 0xaa55 ; boot sector number
+; zero padding and boot sector number
+times 510-($-$$) db 0
+dw 0xaa55
